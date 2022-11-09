220781240e82e0d5e4ca4fa327e3ca70e893cdbf
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-11-09 13:14:10 -0600

    Update controller and view

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -4,13 +4,38 @@ declare(strict_types=1);
 
 namespace Application\Controller;
 
-use Laminas\Mvc\Controller\AbstractActionController;
+use Laminas\Session\Container as SessionContainer;
 use Laminas\View\Model\ViewModel;
+use Laminas\Mvc\Controller\AbstractActionController;
 
 class IndexController extends AbstractActionController
 {
+    /** @var SessionContainer */
+    private $sessionContainer;
+
+    public function __construct(SessionContainer $sessionContainer)
+    {
+        $this->sessionContainer = $sessionContainer;
+    }
+
     public function indexAction()
     {
-        return new ViewModel();
+        return new ViewModel([
+            'updated_at' => $this->sessionContainer->cacheStorage ?? -1
+        ]);
+    }
+
+    public function updateAction()
+    {
+        $this->sessionContainer->cacheStorage = time();
+
+        return $this->redirect()->toRoute('home');
+    }
+
+    public function deleteAction()
+    {
+        $this->sessionContainer->cacheStorage = null;
+
+        return $this->redirect()->toRoute('home');
     }
 }
